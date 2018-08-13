757b84783c6de652418627ff50fe1201d87d5303
snower <sujian199@gmail.com>
2018-08-13 17:07:43 +0800

    fix main add argparse

--- syncany/main.py
+++ syncany/main.py
@@ -2,11 +2,15 @@
 # 18/8/6
 # create by: snower
 
-import sys
+import argparse
 from .taskers.json_tasker import JsonTasker
 
 def main():
-    tasker = JsonTasker(sys.argv[1])
+    parser = argparse.ArgumentParser(description='syncany')
+    parser.add_argument("json", type=str, nargs=argparse.OPTIONAL, help="json filename")
+    args = parser.parse_args()
+
+    tasker = JsonTasker(args.json)
     tasker.run()
 
 if __name__ == "__main__":
