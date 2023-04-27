64806355540468642dcd923d57e6dee36322c888
snower <sujian199@gmail.com>
2023-04-27 18:06:03 +0800

    Optimize import python module

--- syncany/calculaters/import_calculater.py
+++ syncany/calculaters/import_calculater.py
@@ -8,45 +8,56 @@ from ..logger import get_logger
 from .calculater import Calculater
 
 
+IMPORT_MODULES = {}
+
+
 class ImportCalculater(Calculater):
-    def calculate(self, *args):
+    def __init__(self, *args):
+        super(ImportCalculater, self).__init__(*args)
+
         if len(self._import_name) + 2 < len(self.name):
-            calculate_name = self.name[(len(self._import_name) + 2):]
-            attr_names = calculate_name.split(".")
-            module_or_func = self._import_module
+            self.calculate_name = self.name[(len(self._import_name) + 2):]
+            attr_names = self.calculate_name.split(".")
+            self.module_or_func = self._import_module
             for attr_name in attr_names:
-                if not hasattr(module_or_func, attr_name):
-                    raise NotImplementedError("%s not implemented %s" % (self._import_module, calculate_name))
-                module_or_func = getattr(module_or_func, attr_name)
-            if not callable(module_or_func):
-                if not args:
-                    return module_or_func
-                raise NotImplementedError("%s not callable %s" % (self._import_module, calculate_name))
+                if not hasattr(self.module_or_func, attr_name):
+                    raise NotImplementedError("%s not implemented %s" % (self._import_module, self.calculate_name))
+                self.module_or_func = getattr(self.module_or_func, attr_name)
         else:
-            calculate_name = self.name
+            self.calculate_name = self.name
             if not callable(self._import_module):
                 raise NotImplementedError("%s not callable %s" % (self._import_module, self.name))
-            module_or_func = self._import_module
+            self.module_or_func = self._import_module
 
+    def calculate(self, *args):
         try:
             if not args:
-                return module_or_func()
+                if not callable(self.module_or_func):
+                    return self.module_or_func
+                return self.module_or_func()
+
+            if not callable(self.module_or_func):
+                raise NotImplementedError("%s not callable %s" % (self._import_module, self.calculate_name))
             if len(args) == 1 and isinstance(args[0], list) and args[0] and isinstance(args[0][0], dict):
                 try:
-                    return module_or_func(*tuple(args[0]))
+                    return self.module_or_func(*tuple(args[0]))
                 except TypeError:
                     pass
-            return module_or_func(*args)
+            return self.module_or_func(*args)
         except Exception as e:
-            get_logger().warning("import calculater execute %s(%s) error: %s\n%s", calculate_name, args, e,
+            get_logger().warning("import calculater execute %s(%s) error: %s\n%s", self.calculate_name, args, e,
                                  traceback.format_exc())
             return None
 
 
 def create_import_calculater(name, module_or_func):
+    module_id = (name, id(module_or_func))
+    if module_id in IMPORT_MODULES:
+        return IMPORT_MODULES[module_id]
     class_name = "".join([n[:1].upper() + n[1:] for n in name.split("_")]) + "ImportCalculater"
     if isinstance(module_or_func, (types.FunctionType, types.BuiltinFunctionType, types.LambdaType)):
         import_module = lambda self, *args, **kwargs: module_or_func(*args, **kwargs)
     else:
         import_module = module_or_func
-    return type(class_name, (ImportCalculater,), dict(_import_name=name, _import_module=import_module))
+    IMPORT_MODULES[module_id] = type(class_name, (ImportCalculater,), dict(_import_name=name, _import_module=import_module))
+    return IMPORT_MODULES[module_id]
