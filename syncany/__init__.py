7f407900721b6c30e44d5674f81b9de622c34c09
snower <sujian199@gmail.com>
2023-05-08 15:05:55 +0800

    change version to 0.2.9

--- syncany/__init__.py
+++ syncany/__init__.py
@@ -2,8 +2,8 @@
 # 18/8/6
 # create by: snower
 
-version = "0.2.8"
-version_info = (0, 2, 8)
+version = "0.2.9"
+version_info = (0, 2, 9)
 
 from .loaders import Loader, register_loader
 from .outputers import Outputer, register_outputer
