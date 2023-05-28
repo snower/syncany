bbb848fc11e6eb1d99fb245efb689cde4e9c330c
snower <sujian199@gmail.com>
2023-05-28 21:37:10 +0800

    optimize const valuer init

--- syncany/valuers/const.py
+++ syncany/valuers/const.py
@@ -2,14 +2,19 @@
 # 18/8/6
 # create by: snower
 
+import datetime
+from ..utils import ensure_timezone
 from .valuer import Valuer
 
 
 class ConstValuer(Valuer):
     def __init__(self, value, *args, **kwargs):
+        self.value = value
         super(ConstValuer, self).__init__(*args, **kwargs)
 
-        self.value = self.do_filter(value)
+    def new_init(self):
+        super(ConstValuer, self).new_init()
+        self.value = self.do_filter(self.value)
 
     def mount_loader(self, is_return_getter=False, **kwargs):
         pass
@@ -25,6 +30,13 @@ class ConstValuer(Valuer):
 
     def fill_get(self, data):
         return self.value
+
+    def do_filter(self, value):
+        if not self.filter:
+            if isinstance(value, datetime.datetime):
+                value = ensure_timezone(value)
+            return value
+        return self.filter.filter(value)
 
     def require_loaded(self):
         return False
