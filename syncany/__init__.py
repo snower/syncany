6dd50943978b350f517aef9c3945316b213a9375
snower <sujian199@gmail.com>
2023-08-15 16:48:16 +0800

    change version to 0.2.15

--- syncany/__init__.py
+++ syncany/__init__.py
@@ -2,8 +2,8 @@
 # 18/8/6
 # create by: snower
 
-version = "0.2.14"
-version_info = (0, 2, 14)
+version = "0.2.15"
+version_info = (0, 2, 15)
 
 from .loaders import Loader, register_loader
 from .outputers import Outputer, register_outputer
