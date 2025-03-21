0b42d56abce58ed8abe18a2df3b908e336cd9ced
sujian <sujian@aecg.com.cn>
2025-03-21 10:50:36 +0800

    change version to 0.2.22

--- syncany/__init__.py
+++ syncany/__init__.py
@@ -2,8 +2,8 @@
 # 18/8/6
 # create by: snower
 
-version = "0.2.21"
-version_info = (0, 2, 21)
+version = "0.2.22"
+version_info = (0, 2, 22)
 
 from .loaders import Loader, register_loader
 from .outputers import Outputer, register_outputer
